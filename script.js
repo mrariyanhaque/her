b7e9412a31e7639cf86f459d50d408e69ecc81b8
Ariyan Haque <121840774+mrariyanhaque@users.noreply.github.com>
2024-08-07 21:39:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ document.getElementById('revealButton').addEventListener('click', function() {
     document.getElementById('proposalMessage').classList.remove('hidden');
     var music = document.getElementById('proposalMusic');
     music.play();
+    document.getElementById('stopButton').classList.remove('hidden');
+});
+
+document.getElementById('stopButton').addEventListener('click', function() {
+    var music = document.getElementById('proposalMusic');
+    music.pause();
+    music.currentTime = 0; // Optionally reset the music to the beginning
+    document.getElementById('stopButton').classList.add('hidden');
 });

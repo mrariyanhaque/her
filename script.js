d03598a3109d5ffe288830288ec0fa09634d9101
Ariyan Haque <121840774+mrariyanhaque@users.noreply.github.com>
2024-08-07 21:30:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('revealButton').addEventListener('click', function() {
     document.getElementById('proposalMessage').classList.remove('hidden');
+    var music = document.getElementById('proposalMusic');
+    music.play();
 });
